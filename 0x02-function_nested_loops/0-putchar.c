389cceed3fb98b46035ab9f54c172b196ae45d77
OLUWASEYI DAUDA YESUFU <102301571+Odsyesufu7@users.noreply.github.com>
2022-04-13 04:14:12 +0100

    Create 0-putchar.c

---
+++ 0x02-function_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+/**
+ * main - Entry point
+ * @void: no argument
+ * Return: 0
+ **/
+
+int main(void)
+{
+        char ermias[] = "_putchar";
+
+        int c;
+
+        for ( = 0; c < 8; c++)
+
+        {
+                _putchar(ermias[c]);
+        }
+        _putchar('\n');
+        return (0);
+}
