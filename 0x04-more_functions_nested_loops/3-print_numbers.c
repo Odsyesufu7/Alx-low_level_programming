b0ad4ee68d719552bd3a91c98606a4cd6a66bc4b
Syi19 <oyesufu7@live.com>
2022-04-15 12:21:55 +0000

    Print num

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,16 +3,17 @@
  * print_numbers - print numbers
  * Return: Always 0.
  */
-void print_numbers(void)
+void print_numbers(int n)
 {
-int i;
-
-i = 0;
-while (i < 10)
-{
-_putchar (i + '0');
-i++;
-}
-
-_putchar ('\n');
+	unsigned int x = n;
+	if (n < 0)
+	{
+		_putchar(45);
+		x = -x;
+	}
+	if ((x / 10) > 0)
+	{
+		print_numbers(x / 10);
+	}
+	_putchar((x % 10) + 48);
 }
