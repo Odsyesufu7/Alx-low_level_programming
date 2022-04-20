8716ad17e97a872913eb962c115a9ef411bb839c
Syi19 <oyesufu7@live.com>
2022-04-20 15:26:08 +0000

    My commit msg

---
+++ 0x05-pointers_arrays_strings/0-rest_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * rest_to_98 - updates a integer value
+ * @n: A pointer to an int that will be updated
+ *
+ * Return: Void that means is correct
+ */
+
+void reset_to_98(int *n)
+
+{
+*n = 98;
+}
