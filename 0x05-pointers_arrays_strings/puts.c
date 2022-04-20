93ebb3c9f081ab926332d0b0d5e6ab9a03a5440b
Syi19 <oyesufu7@live.com>
2022-04-20 18:50:38 +0000

    My third commit task

---
+++ 0x05-pointers_arrays_strings/puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*_puts - prints a string
+*@str: A pointer to an int that will be changed
+*
+*Return void which means our answer is correct
+*/
+
+void _puts(char *str)
+{
+char *c;
+int x;
+
+x = str;
+
+for (n = 0; c[x]; x++)
+{
+_putchar (c[x]);
+}
+_putchar('\n');
+}
