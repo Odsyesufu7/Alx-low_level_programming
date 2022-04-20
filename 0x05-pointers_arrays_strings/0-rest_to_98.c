c918ca3111b9ac387bf46089850bb76005fc3b98
Syi19 <oyesufu7@live.com>
2022-04-20 16:23:38 +0000

    My zero task commit

--- 0x05-pointers_arrays_strings/0-rest_to_98.c
+++ 0x05-pointers_arrays_strings/0-rest_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * rest_to_98 - updates a integer value
- * @n: A pointer to an int that will be updated
+ *rest_to_98 - updates a integer value 98
+ *@n: A pointer to an int that will be updated
  *
- * Return: Void that means is correct
+ *Return: Void that means is correct
  */
 
 void reset_to_98(int *n)
