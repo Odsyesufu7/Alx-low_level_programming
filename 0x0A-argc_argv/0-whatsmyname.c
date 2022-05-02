1d6aa5dfb59cb27ef0d5705f4c60fdb8f024c8ba
OLUWASEYI DAUDA YESUFU <102301571+Odsyesufu7@users.noreply.github.com>
2022-05-02 22:08:32 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,18 @@
-#include<stdio.h>
-#define UNUSED(x) (void)(x)
+#include <stdio.h>
+#include "main.h"
 
 /**
-* main - function
-*
-* @argv: the chaine of chaine
-* @argc: the int
-* Return: Always 0.
-*/
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
+ */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	UNUSED(argc);
-	printf("%s\n", argv[0]);
+(void) argc;
+printf("%s\n", argv[0]);
 
-	return (0);
+return (0);
 }
