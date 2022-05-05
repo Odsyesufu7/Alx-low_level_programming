076c063cad720a5906bd3b5c58b83069a3e06821
Syi19 <oyesufu7@live.com>
2022-05-05 10:19:06 +0000

    My final task commit

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,7 +9,7 @@ char **strtow(char *str)
 {
 	char **ptr;
 	int i, k, len, start, end, j = 0;
-	int words =  countWords(str);
+	int words = countWords(str);
 
 	if (!str || !countWords(str))
 		return (NULL);
@@ -18,8 +18,8 @@ char **strtow(char *str)
 		return (NULL);
 	for (i = 0; i < words; i++)
 	{
-		start = startIndex(str, j);
-		end = endIndex(str, start);
+		start = StartIndex(str, j);
+		end = EndIndex(str, start);
 		len = end - start;
 		ptr[i] = malloc(sizeof(char) * (len + 1));
 		if (!ptr[i])
