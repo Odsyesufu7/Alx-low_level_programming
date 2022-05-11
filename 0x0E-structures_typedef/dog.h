0d6027ca573d72bac61884680073dc3ee8fefa01
OLUWASEYI DAUDA YESUFU <102301571+Odsyesufu7@users.noreply.github.com>
2022-05-11 14:28:25 +0100

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,3 @@
-
 #ifndef DOG_H
 #define DOG_H
 
@@ -6,15 +5,15 @@
  * struct dog - Struct dog
  * @name: name
  * @age: age
- * @owner: owner
+ * @owner:  owner
  * Description: This struct is for dog
  */
 
 struct dog
 {
-char *name;
-float age;
-char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 typedef struct dog dog_t;
@@ -24,4 +23,3 @@ dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
 #endif /*DOG_H*/
-
