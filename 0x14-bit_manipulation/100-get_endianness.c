d5548087c4101300e6c4c7b190617733398f717d
OLUWASEYI DAUDA YESUFU <102301571+Odsyesufu7@users.noreply.github.com>
2022-06-01 12:50:04 +0100

    Update 100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * get_endianness - checks if a machine is little or big endian
- * 
- * Return: 0 for big endian, 1 for little endian
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
  */
 int get_endianness(void)
 {
-    unsigned int i;
-    char *c;
+	unsigned int x;
+	char *c;
 
-    i = 1;
-    c = (char *) &i;
-    
-    return ((int)*c);
+	x = 1;
+	c = (char *) &x;
+
+	return ((int)*c);
 }
